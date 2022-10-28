54827cfab73ac38e7f60cd537b0937a34fc22723
dragonflyinmypool <jheiney10@gmail.com>
2022-10-28 13:59:42 +0100

    settings now save to local storage

--- src/stores/settingsStore.js
+++ src/stores/settingsStore.js
@@ -1,10 +1,19 @@
 import { writable } from "svelte/store";
 
-export const setting = writable({
+const defaultSettings = {
   showPickedBalls: true,
   numberOfBalls: 75,
   repeatCall: false,
   lang1: "None",
   lang2: "None",
   lang3: "None",
+};
+
+// Check local storage for settings
+const storedSettings = JSON.parse(localStorage.getItem("settings"));
+export const setting = writable(storedSettings || defaultSettings);
+
+// Save settings to local storage
+setting.subscribe((value) => {
+  localStorage.setItem("settings", JSON.stringify(value));
 });
